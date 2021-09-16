ca42c9357cc2ef5204e5faaee496c53214ee267f
jaffasunny <jaffer.sunny125@gmail.com>
2021-09-16 13:07:20 +0500

    simple react layout with no components

--- src/App.js
+++ src/App.js
@@ -1,41 +1,80 @@
-import React from 'react';
+import React from "react";
 import classes from "./App.module.css";
-import ProductData from './ProductData';
-
+import ProductData from "./ProductData";
 
 function App() {
-	const currHour = new Date().getHours() > 9 ? new Date().getHours() : '0' + new Date().getHours();
-	const currMinutes = new Date().getMinutes() > 9 ? new Date().getMinutes() : '0' + new Date().getMinutes();
+	const currHour =
+		new Date().getHours() > 9
+			? new Date().getHours()
+			: "0" + new Date().getHours();
+	const currMinutes =
+		new Date().getMinutes() > 9
+			? new Date().getMinutes()
+			: "0" + new Date().getMinutes();
 	return (
 		<div className="App">
 			<header className="App-header">
 				<nav className={classes.TopBar}>
-					<img src="https://i.dlpng.com/static/png/197684_preview.png" alt="Amazon Logo" />
+					<img
+						src="./amazon_logo.png"
+						alt="Amazon Logo"
+					/>
 				</nav>
 			</header>
 
 			<div className={classes.MainContainer}>
 				<div className={classes.ProductPreview}>
 					<img src="https://imgur.com/iOeUBV7.png" alt="" />
-					<div className={classes.featureData}>
+					{/* <div className={classes.TimeSection}>
 						<p>{`${currHour}:${currMinutes}`}</p>
+					</div> */}
+					<div className={classes.HeartSection}>
+						<i class="fas fa-heartbeat"></i>
+						<p>78</p>
 					</div>
 				</div>
 				<div className={classes.ProductData}>
 					<h1 className={classes.ProductTitle}>{ProductData.title}</h1>
-					<p className={classes.ProductDescription}>{ProductData.description}</p>
+					<p className={classes.ProductDescription}>
+						{ProductData.description}
+					</p>
 
 					<h3 className={classes.SectionHeading}>Select Color</h3>
 					<div>
-						<img className={[classes.ProductImage, classes.SelectedProductImage].join(' ')} src={ProductData.colorOptions[0].imageUrl} alt={ProductData.colorOptions[0].styleName} />
-						<img className={classes.ProductImage} src={ProductData.colorOptions[1].imageUrl} alt={ProductData.colorOptions[1].styleName} />
-						<img className={classes.ProductImage} src={ProductData.colorOptions[2].imageUrl} alt={ProductData.colorOptions[2].styleName} />
-						<img className={classes.ProductImage} src={ProductData.colorOptions[3].imageUrl} alt={ProductData.colorOptions[3].styleName} />
+						<img
+							className={[
+								classes.ProductImage,
+								classes.SelectedProductImage,
+							].join(" ")}
+							src={ProductData.colorOptions[0].imageUrl}
+							alt={ProductData.colorOptions[0].styleName}
+						/>
+						<img
+							className={classes.ProductImage}
+							src={ProductData.colorOptions[1].imageUrl}
+							alt={ProductData.colorOptions[1].styleName}
+						/>
+						<img
+							className={classes.ProductImage}
+							src={ProductData.colorOptions[2].imageUrl}
+							alt={ProductData.colorOptions[2].styleName}
+						/>
+						<img
+							className={classes.ProductImage}
+							src={ProductData.colorOptions[3].imageUrl}
+							alt={ProductData.colorOptions[3].styleName}
+						/>
 					</div>
 
 					<h3 className={classes.SectionHeading}>Features</h3>
 					<div>
-						<button className={[classes.FeatureItem, classes.SelectedFeaturedItem].join(' ')}>Time</button>
+						<button
+							className={[
+								classes.FeatureItem,
+								classes.SelectedFeaturedItem,
+							].join(" ")}>
+							Time
+						</button>
 						<button className={classes.FeatureItem}>Heart Rate</button>
 					</div>
 					<button className={classes.PrimaryButton}>Buy Now</button>
